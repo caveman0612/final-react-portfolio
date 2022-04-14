baddbbb67c932676969bab96778be39309cb26c2
cavem <kyle.stinar@hotmail.com>
2022-04-13 17:19:30 -0700

    adding header image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./main/About";
+import Footer from "./main/Footer";
+import Header from "./main/Header";
+import Projects from "./main/Projects";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Projects />
+      <About />
+      <Footer />
     </div>
   );
 }
