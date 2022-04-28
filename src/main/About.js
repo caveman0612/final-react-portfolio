dbb840557a7b8d12f38ca84c64cd368783f8a767
cavem <kyle.stinar@hotmail.com>
2022-04-28 07:22:35 -0700

    cleaning up

--- src/main/About.js
+++ src/main/About.js
@@ -23,26 +23,29 @@ const About = () => {
             <li>I like working with computers.</li>
           </ol>
         </div> */}
-        <p></p>
+        {/* <p>I found programming when the pandemic first started. I started off with python as a fun hobby, but about a year ago I realized that I wanted to do this as a career. I started teaching myself the basics of web design, like </p> */}
         <p>
           I have always been good with computers, but in college, I discovered
           programming. After a few weeks I feel in love with it. I had been
           doing it for fun since the pandemic started, but about a year ago, I
           realized that I wanted to do this as a career.
         </p>
+        <br />
         <p>
           From my time in the military and being a math tutor I am very
           disciplined which has allowed me to teach myself a fair amount of
           programming fundamentals, but I am hoping Thinkful can fill the gaps
           in my knowledge.
         </p>
+        <br />
         <p>
           Over the years I have had the oppertunity to work with many people
           from all over the world. This gave me the oppurtunity to learn how to
           be flexible to a variety of team dynamics.
         </p>
+        <br />
         <p>
-          When I am not programming I like to make stuff. I have been doing
+          When I am not programming I like to craft stuff. I have been doing
           carpentry and woodcarving since I was a very young, but over the
           passed few years I have started getting into 3d printing and learning
           how to 3d model using CAD software.
